089b6555b31fe103c6cf5427c07ef43db0cbcfeb
emre <emrebaglayici1@gmail.com>
2022-01-22 17:59:15 +0300

    Starting the project

---
+++ src/main/java/HRMS/HRMS/Project/HrmsProjectApplication.java
@@ -0,0 +1,13 @@
+package HRMS.HRMS.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsProjectApplication.class, args);
+	}
+
+}
